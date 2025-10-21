29501a21db927dcb17dd303a43a9972396e7061b
bbabpuli <bbabpuli@gmail.com>
2025-10-21 18:01:34 +0900

    feat: Blog Ranking Service

---
+++ src/main/java/com/ktdsuniversity/edu/domain/blog/controller/SearchBlogController.java
@@ -0,0 +1,62 @@
+package com.ktdsuniversity.edu.domain.blog.controller;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.ktdsuniversity.edu.domain.blog.service.BlogRankService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+
+/************************************************
+ * 1. 블로그 검색 API 호출 및 사용자의 블로그 순위 조회
+ * 2. 블로그 일일 방문자 수 조회 API 호출 및 DB 저장
+ * 
+ * 
+ * @author 동은
+ ************************************************/
+@RestController
+public class SearchBlogController {
+	
+	private static final Logger log = LoggerFactory.getLogger(SearchBlogController.class);
+	
+	 @Autowired
+	    BlogRankService rankService;
+
+	    /**
+	     * 블로그 검색 API 호출 및 사용자의 블로그 순위 조회
+	     * @param query 검색어
+	     * @param userBlogLink 사용자의 블로그 링크
+	     * @return 사용자의 블로그 순위와 API 응답 결과
+	     */
+	    @GetMapping(value="/blog/rank")
+	    public ObjectNode getBlogRank(
+	            @RequestParam String query,
+	            @RequestParam String userBlogLink) {
+
+	        log.info("검색 파라미터: query={}, display={}, start={}, sort={}, userBlogLink={}",
+	                query, userBlogLink);
+
+	        // API 응답 전체와 블로그 순위를 반환
+	        ObjectNode result = rankService.searchBlogAndFindUserRank(query, userBlogLink);
+
+	        return result;  // JSON 응답으로 전체 API 결과와 순위 정보 반환
+	    }
+	    
+	    
+	    
+		
+		// TODO: 블로그 일일 방문자 수 조회 API 호출 및 DB 저장
+		// https://blog.naver.com/NVisitorgp4Ajax.nhn?blogId={blogId}
+		
+	
+	
+	
+}
